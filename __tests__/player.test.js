0794822c411d1d79c17a9b04942b91cfd8b8657e
unknown <amadocardenas0@gmail.com>
2021-03-25 22:12:00 -0600

    saving progress so I can go eat

---
+++ __tests__/player.test.js
@@ -0,0 +1,15 @@
+const Player = require('../lib/player');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+test('creates a player object', () => {
+    const player = new Player('Dave');
+
+    console.log(player);
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+});
